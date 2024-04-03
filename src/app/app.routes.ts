da05d0220823cea58ed4167cd612eb41d5097095
HaroldContrerasCardona <h.contrerascardona@dxc.com>
2024-04-02 23:56:11 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
+import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('@users/pages/list/list.component').then(m => m.ListComponent)
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
---
+++ src/app/domains/shared/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterLinkWithHref],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
